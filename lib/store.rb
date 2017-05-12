b8ff610ffafab1ee3a66fc128e816caa4f22aa9f
Jasonlazzuri <jason.lazzuri@gmail.com>
2017-05-12 11:55:31 -0700

    right before lunch stop

--- lib/store.rb
+++ lib/store.rb
@@ -1,12 +1,8 @@
 class Store < ActiveRecord::Base
   has_many :middles
   has_many :brands, through: :middles
-  validates(:name, {:presence => true, :length => {:maximum => 100 }})
+  validates(:name, {:uniqueness => true, :presence => true, :length => {:maximum => 100 }})
   before_save(:upcase_name)
-
-  scope(:not_done, -> do
-  where({:done => false})
-  end)
 
   private
     define_method(:upcase_name)do
